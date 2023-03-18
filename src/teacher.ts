018fffa6f69898461104985380fee94f0ec8baf6
Blazej Sak <blazej.sak@icloud.com>
2023-03-18 16:17:46 +0100

    Fix Teacher parser

--- src/teacher.ts
+++ src/teacher.ts
@@ -1,61 +1,89 @@
 class Teacher {
-    title: string = '';
-    firstName: string = '';
-    middleName: string = '';
-    lastName: string = '';
-    note: string = '';
+  title: string = "";
+  firstName: string = "";
+  middleName: string = "";
+  lastName: string = "";
+  note: string = "";
 
-    toString(){
-      return `${this.title} ${this.firstName} ${this.lastName}`.trim();
-    }
+  toString() {
+    return `${this.title} ${this.firstName} ${this.lastName}`.trim();
+  }
 
-    usosName() {
-      return `${this.firstName} ${this.lastName}`;
-    }
+  usosName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 
-    wdName() {
-      return `${this.title} ${this.lastName} ${this.firstName} ${this.middleName}`.trim();
-    }
+  wdName() {
+    return `${this.title} ${this.lastName} ${this.firstName} ${this.middleName}`.trim();
+  }
 
-    // TODO edge case 'dr hab. prof. SGH lastName firstName'
-    static parseTeacherFromWD(name: string) {
-        let array = name.split(' ')
+  // ugly but working (does not remove ',' character yet)
+  static parseTeacherFromWD(name: string) {
+    let array = name.split(" ");
 
-        let teacher = new Teacher();
+    let teacher = new Teacher();
 
-        array.forEach((word, cur, arr) => {
-          if (word.startsWith('mgr') || word.startsWith('dr') || word.endsWith('.')) {
-      
-            if (teacher.lastName || (teacher.lastName && teacher.firstName)) {
-              teacher.note += ` ${teacher.lastName} ${teacher.firstName}`;
-              teacher.lastName = '';
-            }
-      
-            if (teacher.title) teacher.title += ` ${word}`;
-            else teacher.title = `${word}`;
-      
-            return;
-          }
-      
-          if (!teacher.lastName) {
-            teacher.lastName = word;
-            return;
-          }
-          else if (!teacher.firstName) {
-            teacher.firstName = word;
-            return;
-          }
-          else if (!teacher.middleName) {
-            teacher.middleName = word;
-            return;
-          }
-      
-          if (teacher.note) teacher.note += ` ${word}`;
-          else teacher.note = `${word}`;
-        });
-      
-        return teacher;
+    array.forEach((word, cur, arr) => {
+      if (teacher.title.endsWith(word)) return;
+
+      if (word.startsWith("prof")) {
+        let next = arr[cur + 1]; console.log(word, next);
+
+        if (
+          next.startsWith("mgr") ||
+          next.startsWith("dr") ||
+          next.endsWith(".")
+        ) {
+          if (teacher.title) teacher.title += ` ${word}`;
+          else teacher.title = `${word}`;
+          return;
+        } else {
+          if (teacher.title) teacher.title += ` ${word} ${next}`;
+          else teacher.title = `${word} ${next}`;
+          return;
+        }
       }
+
+      if (
+        word.startsWith("mgr") ||
+        word.startsWith("dr") ||
+        word.endsWith(".")
+      ) {
+        if (teacher.lastName && teacher.firstName) {
+          teacher.note += ` ${word}`;
+          return;
+        }
+
+        if (teacher.lastName) {
+          teacher.note += ` ${teacher.lastName} ${teacher.firstName}`;
+          teacher.lastName = "";
+        }
+
+        if (teacher.title) teacher.title += ` ${word}`;
+        else teacher.title = `${word}`;
+
+        return;
+      }
+
+      if (teacher.note) teacher.note += ` ${word}`;
+      else {
+        if (!teacher.lastName) {
+          teacher.lastName = word;
+          return;
+        } else if (!teacher.firstName) {
+          teacher.firstName = word;
+          return;
+        } else if (!teacher.middleName) {
+          teacher.middleName = word;
+          return;
+        }
+
+        teacher.note = `${word}`;
+      }
+    });
+
+    return teacher;
+  }
 }
 
-export { Teacher }
+export { Teacher };
